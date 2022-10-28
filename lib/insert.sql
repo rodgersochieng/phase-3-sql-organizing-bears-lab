d609ac89d5ffe822a51e95b52aacc4614b10af40
rodgersochieng <rodger.ochieng@student.moringaschool.com>
2022-10-28 17:07:47 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr.Chocolate',62, 'M','Red','Calm', TRUE),
+('Rowdy',60, 'F','Green','Calm', TRUE),
+('Tabitha',56, 'F','Red','vigor', FALSE),
+('Sergeant Brown',63, 'M','Red','Calm', TRUE),
+('Melissa',62, 'F','Red','Calm', TRUE),
+('Grinch',60, 'M','Red','Calm', TRUE),
+('Wendy',58, 'F','Pink','Calm', TRUE),
+(NULL,54, 'M','Grey','Calm', TRUE)
